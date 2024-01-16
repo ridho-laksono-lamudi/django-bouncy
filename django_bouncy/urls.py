24e5d505d7f2fca24a649af854f17c1c939c282d
Alex Zhang <alex.zhang@wspdigital.com>
2024-01-17 10:17:29 +1100

    Bump Django from 1.11.23 to 4.2.9

--- django_bouncy/urls.py
+++ django_bouncy/urls.py
@@ -1,8 +1,8 @@
 """URLs for the Django-Bouncy App"""
-from django.conf.urls import url
+from django.urls import re_path
 # pylint: disable=invalid-name
 from django_bouncy.views import endpoint
 
 urlpatterns = [
-    url(r'^$', endpoint)
+    re_path(r'^$', endpoint)
 ]
