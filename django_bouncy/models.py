8f59948a5e16b2c203f4065bb6d5435a270a0c8c
ridho <ridho.laksono@lamudi.com>
2025-09-22 14:24:26 +0700

    compat

--- django_bouncy/models.py
+++ django_bouncy/models.py
@@ -1,10 +1,9 @@
 """Models for the django_bouncy app"""
-from __future__ import unicode_literals
-
+    
 from django.db import models
-from django.utils.six import python_2_unicode_compatible
-
-
+    
+    
+    
 class Feedback(models.Model):
     """An abstract model for all SES Feedback Reports"""
     created_at = models.DateTimeField(auto_now_add=True)
@@ -19,13 +18,12 @@ class Feedback(models.Model):
     feedback_id = models.CharField(max_length=100, null=True, blank=True)
     feedback_timestamp = models.DateTimeField(
         verbose_name="Feedback Time", null=True, blank=True)
-
+    
     class Meta(object):
         """Meta info for Feedback Abstract Model"""
         abstract = True
-
-
-@python_2_unicode_compatible
+    
+    
 class Bounce(Feedback):
     """A bounce report for an individual email address"""
     hard = models.BooleanField(db_index=True, verbose_name="Hard Bounce")
@@ -41,14 +39,12 @@ class Bounce(Feedback):
     status = models.CharField(
         db_index=True, null=True, blank=True, max_length=150)
     diagnostic_code = models.TextField(null=True, blank=True, max_length=5000)
-
+    
     def __str__(self):
         """Unicode representation of Bounce"""
-        return "%s %s Bounce (message from %s)" % (
-            self.address, self.bounce_type, self.mail_from)
-
-
-@python_2_unicode_compatible
+        return f"{self.address} {self.bounce_type} Bounce (message from {self.mail_from})"
+    
+    
 class Complaint(Feedback):
     """A complaint report for an individual email address"""
     useragent = models.TextField(blank=True, null=True)
@@ -57,25 +53,22 @@ class Complaint(Feedback):
         verbose_name="Complaint Type"
     )
     arrival_date = models.DateTimeField(blank=True, null=True)
-
+    
     def __str__(self):
         """Unicode representation of Complaint"""
-        return "%s Complaint (email sender: from %s)" % (
-            self.address, self.mail_from)
-
-
-@python_2_unicode_compatible
+        return f"{self.address} Complaint (email sender: from {self.mail_from})"
+    
+    
 class Delivery(Feedback):
     """A delivery report for an individual email address"""
     delivered_time = models.DateTimeField(blank=True, null=True)
     processing_time = models.IntegerField(default=0)
     smtp_response = models.TextField(blank=True, null=True)
-
+    
     def __str__(self):
         """Unicode representation of Delivery"""
-        return "%s Delivery (email sender: from %s)" % (
-            self.address, self.mail_from)
-
+        return f"{self.address} Delivery (email sender: from {self.mail_from})"
+    
     class Meta(object):
         """Meta info for the Delivery model"""
         verbose_name_plural = 'deliveries'
